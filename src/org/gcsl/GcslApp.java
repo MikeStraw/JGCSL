2ab41da8044bee1929bbcab5832ea33b4ec742ac
Mike Straw <mstraw@columbus.rr.com>
2017-09-04 20:27:00 -0400

    Add menu, image and status bar.  Resize of image is still not correct.

--- src/org/gcsl/GcslApp.java
+++ src/org/gcsl/GcslApp.java
@@ -1,22 +1,31 @@
 package org.gcsl;
 
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import org.gcsl.View.RootLayoutController;
 
 import java.io.IOException;
 
-class GcslApp
+public class GcslApp
 {
+    private RootLayoutController gcslAppController;
     private Stage stage;
 
     GcslApp(Stage primaryStage, String fxmlPath) throws IOException
     {
+        FXMLLoader loader = new FXMLLoader();
+
+
+        loader.setLocation(getClass().getResource(fxmlPath));
+        BorderPane root = loader.load();
+        gcslAppController = loader.getController();
+        gcslAppController.setApp(this);  // allow communication from controller back to app
+
         stage = primaryStage;
-        Parent root = FXMLLoader.load(getClass().getResource(fxmlPath));
         stage.setTitle("GCSL Registrar");
-        stage.setScene(new Scene(root, 600, 400));
+        stage.setScene(new Scene(root, 525, 405));
     }
 
     void start()
--- src/org/gcsl/View/RootLayoutController.java
+++ src/org/gcsl/View/RootLayoutController.java
@@ -1,5 +1,52 @@
 package org.gcsl.View;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import org.gcsl.GcslApp;
+
 public class RootLayoutController
 {
+    // TODO:  HOw to make image resize properly.  BorderPane has issues with not clipping content.
+    //        Possibly try AnchorPane with VBox -OR- nest HBox inside center of BorderPane.
+    private GcslApp app;
+
+    @FXML private ImageView imageView;
+    @FXML private Label status;
+
+    @FXML private void handleProcessResults(ActionEvent event)
+    {
+        System.out.println("Process Results Clicked.");
+    }
+
+    @FXML private void handleProcessRosters(ActionEvent event)
+    {
+        System.out.println("Process Rosters Clicked.");
+    }
+
+    @FXML private void handleReportsChampsExceptions(ActionEvent event)
+    {
+        System.out.println("Reports Champs Exceptions Clicked.");
+    }
+
+    @FXML private void handleReportsMeetCount(ActionEvent event)
+    {
+        System.out.println("Reports Meet Count Clicked.");
+    }
+
+    @FXML private void handleReportsOrphans(ActionEvent event)
+    {
+        System.out.println("Reports Orphans Clicked.");
+    }
+
+    @FXML private void initialize()
+    {
+        status.setText("Opening SQLite DB ...");
+    }
+
+    public void setApp(GcslApp app)
+    {
+        this.app = app;
+    }
 }
