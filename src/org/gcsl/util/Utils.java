2e76765440dd918560eb642b0e6c169b4d8b1a8e
Mike Straw <mstraw@columbus.rr.com>
2017-09-09 18:24:34 -0400

    Added file and archive file methods to Utils. Update ProcessRosterDialog to read archive dir and display contents.

--- src/org/gcsl/util/Utils.java
+++ src/org/gcsl/util/Utils.java
@@ -1,13 +1,26 @@
 package org.gcsl.util;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
 public class Utils
 {
     private Utils() {}
 
     public final static int INVALID_ID = -1;
 
+    public enum ARCHIVE_FILE_TYPE { ZIP, SD3, UNKNOWN }
+
     // makeDateString - assumes input is mmddyyyy and returns yyyy-mm-dd which is suitable for SQLite
-    public static String makeDateString(String buf) {
+    public static String makeDateString(String buf)
+    {
         String dateStr;
 
         if (buf.length() >= 8) {
@@ -18,5 +31,76 @@ public class Utils
         }
 
         return dateStr;
+    }
+
+    // Determine the type of the file identified by filePath.  This method is only checking for
+    // SD3 and ZIP extensions.
+    public static Utils.ARCHIVE_FILE_TYPE getArchiveFileType(String filePath)
+    {
+        return getArchiveFileType(new File(filePath));
+    }
+
+    public static Utils.ARCHIVE_FILE_TYPE getArchiveFileType(File file)
+    {
+        String fileName = file.getName();
+        ARCHIVE_FILE_TYPE type = ARCHIVE_FILE_TYPE.UNKNOWN;
+
+        if (file.exists()  &&  ! file.isDirectory()  &&  fileName.length() > 4) {
+            if (fileName.substring(fileName.length()-4).equalsIgnoreCase(".sd3")) {
+                type = ARCHIVE_FILE_TYPE.SD3;
+            }
+            else if (fileName.substring(fileName.length()-4).equalsIgnoreCase(".zip")) {
+                type = ARCHIVE_FILE_TYPE.ZIP;
+            }
+        }
+
+        return type;
+    }
+
+    public static List<String> getFileNamesFromArchive(File file)
+    {
+        ARCHIVE_FILE_TYPE archiveType = getArchiveFileType(file);
+        List<String> files = new ArrayList<>();
+
+        if (archiveType == ARCHIVE_FILE_TYPE.SD3) {
+            files.add(file.getName());
+        }
+        else if (archiveType == ARCHIVE_FILE_TYPE.ZIP) {
+            try (ZipFile zipEntryFile = new ZipFile(file.getPath())){
+                final Enumeration<? extends ZipEntry> entries = zipEntryFile.entries();
+
+                while (entries.hasMoreElements()) {
+                    final ZipEntry entry = entries.nextElement();
+                    files.add(entry.getName());
+                }
+            }
+            catch (IOException e) {
+                // suppress error
+                System.err.println("WARNING:  I/O error reading archive: " + e.getMessage());
+            }
+        }
+        return files;
+    }
+
+    public static List<File> getFilesFromDirectory(File dir, List<String>extensions)
+    {
+        List<File> files = new ArrayList<>();
+        for (String extension : extensions) {
+            files.addAll( getFilesFromDirectory(dir, extension) );
+        }
+        return files;
+    }
+
+    public static List<File> getFilesFromDirectory(File dir, String extension)
+    {
+        List<File> files = new ArrayList<>();
+
+        if (dir.isDirectory() && extension.length() > 1) {
+            FilenameFilter filter = (d, name) -> name.toLowerCase().endsWith(extension.toLowerCase());
+
+            files = Arrays.asList(dir.listFiles(filter));
+        }
+
+        return files;
     }
 }
--- src/org/gcsl/view/ProcessRosterDialogController.java
+++ src/org/gcsl/view/ProcessRosterDialogController.java
@@ -1,7 +1,9 @@
 package org.gcsl.view;
 
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
@@ -9,8 +11,15 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.TextField;
 
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import org.gcsl.model.ProcessArchiveItem;
+import org.gcsl.util.Utils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class ProcessRosterDialogController
@@ -26,35 +35,79 @@ public class ProcessRosterDialogController
     @FXML private TableColumn<ProcessArchiveItem, String> contentsColumn;
     @FXML private TableColumn<ProcessArchiveItem, Boolean> selectedColumn;
 
+    private boolean archiveDirIsDirty = false;
     private Stage dialogStage;
-
     private final ObservableList<ProcessArchiveItem> archiveItemsList = FXCollections.observableArrayList();
 
+    // ***************     Public Methods
     public void setDialogStage(Stage stage) { dialogStage = stage; }
     public void setRosterDir(String dir)    { archiveDir.setText(dir); }
 
+    // ***************     Private GUI methods
     @FXML private void initialize()
     {
         System.out.println("Inside ProcessRosterDialogController::initialize");
-        // add some fake data
-        archiveItemsList.add(new ProcessArchiveItem("DARTS-Roster004.ZIP", "CFILE01.CL2, HFILE001.HY3", true));
-        archiveItemsList.add(new ProcessArchiveItem("Archive Name #2", "CFILE01.CL2, HFILE001.HY3", true));
-        archiveItemsList.add(new ProcessArchiveItem("Archive Name #3", "CFILE01.CL2, HFILE001.HY3", true));
-        archiveItemsList.add(new ProcessArchiveItem("Archive Name #4", "CFILE01.CL2, HFILE001.HY3", true));
-        archiveItemsList.add(new ProcessArchiveItem("Archive Name #5", "CFILE01.CL2, HFILE001.HY3", true));
-        archiveItemsList.add(new ProcessArchiveItem("NWGCS-OH-Roster004.ZIP", "CFILE01.CL2, HFILE001.HY3", true));
-        archiveItemsList.add(new ProcessArchiveItem("WAVE-GS-004.sd3", "Worthington_Roster.sd3", true));
 
         archiveTable.setItems(archiveItemsList);
+
+        // set up archive directory textfield listeners
+        // listener called with each change to text field
+        //   + happens when setText called
+        //   + happens with each character typed into field
+        archiveDir.textProperty().addListener( (observable, oldValue, newValue) -> {
+
+            archiveDirIsDirty = true;
+            // For convenience, call populateTable if oldValue is empty (ie during initialization)
+            if (oldValue.isEmpty() &&  ! newValue.isEmpty()) {
+                populateArchiveTable(new File(newValue));
+            }
+        });
+
+        // OnAction happens when Enter hit while focus is in textfield
+//        archiveDir.setOnAction( (event) -> {
+//            System.out.println("archiveDir OnAction");
+//        });
+
+        //
+        archiveDir.focusedProperty().addListener( (ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+            focusState(newValue);
+        });
     }
+
+    private void focusState(boolean hasFocus)
+    {
+        if (! hasFocus  &&  archiveDirIsDirty) {
+            populateArchiveTable(new File(archiveDir.getText()));
+        }
+    }
+
+
+    // Called when users hits Enter from the textfield
+    @FXML private void handleArchiveDirAction(ActionEvent event)
+    {
+        browseBtn.requestFocus();
+        if (archiveDirIsDirty) {
+            populateArchiveTable(new File(archiveDir.getText()));
+        }
+    }
+
 
     // *************** GUI Handler Methods
 
     @FXML private void handleBrowseButtonClick()
     {
-        System.out.println("Browse Button Click");
+        DirectoryChooser dirChooser = new DirectoryChooser();
+        File  initialDir = new File(archiveDir.getText());
 
+        if (initialDir.exists()  &&  initialDir.isDirectory()) {
+            dirChooser.setInitialDirectory(initialDir);
+        }
 
+        File newDir = dirChooser.showDialog(dialogStage.getOwner());
+        if (newDir != null  &&  ! newDir.equals(initialDir)) {
+            archiveDir.setText(newDir.getAbsolutePath());
+            populateArchiveTable(newDir);
+        }
     }
     @FXML private void handleCancelButtonClick()
     {
@@ -71,6 +124,27 @@ public class ProcessRosterDialogController
         System.out.printf("Archive 0: path=%s, contents=%s, selected=%s %n", item.getPath(), item.getContents(), item.getSelected());
     }
 
+    // ***************     Private Methods
+    private void populateArchiveTable(File archiveDir)
+    {
+        // invalidate existing list
+        archiveItemsList.clear();
 
+        // Look for all ZIP and SD3 files
+        String       extensions    = ".sd3 .zip";
+        List<String> extensionList = new ArrayList<>(Arrays.asList(extensions.split(" ")));
+        List<File>   rosterFiles   = Utils.getFilesFromDirectory(archiveDir, extensionList);
+        System.out.printf("populateArchiveTable:  path=%s, num archive files=%d %n",
+                          archiveDir.getPath(), rosterFiles.size());
+
+        for (File file : rosterFiles) {
+            List<String> filesInArchive = Utils.getFileNamesFromArchive(file);
+            String contents = String.join(", ", filesInArchive);
+            archiveItemsList.add(new ProcessArchiveItem(file.getName(), contents, true));
+        }
+
+        archiveDirIsDirty = false;
+        browseBtn.requestFocus();  // remove focus from text field when dialog first displayed
+    }
 
 }
